29c9a053455f1bda289421e0daa67231c2efc404
pathoschild <pathoschild+github@gmail.com>
2012-05-23 10:14:10 -0400

    updated file structure.

--- Pathoschild.Http.Formatters.Core/Properties/AssemblyInfo.cs
+++ Pathoschild.Http.Formatters.Core/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Pathoschild.Http.Formatters.Core")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Provides a base implementation of the .NET MediaTypeFormatter for serializers.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Pathoschild.Http.Formatters.Core")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("Copyright Jesse Plamondon-Willard 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
