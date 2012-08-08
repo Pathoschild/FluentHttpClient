dc716c747d15ca4e1ba4402f6a0c79255e5546ee
Pathoschild <pathoschild+github@gmail.com>
2012-08-08 13:01:10 -0400

    made factory methods virtual for extension.

--- Client/Default/Factory.cs
+++ Client/Default/Factory.cs
@@ -20,7 +20,7 @@ namespace Pathoschild.Http.Client.Default
 		/// <param name="message">The underlying HTTP request message.</param>
 		/// <param name="formatters">The formatters used for serializing and deserializing message bodies.</param>
 		/// <param name="responseBuilder">Executes an HTTP request.</param>
-		public IRequestBuilder GetRequestBuilder(HttpRequestMessage message, MediaTypeFormatterCollection formatters, Func<IRequestBuilder, Task<HttpResponseMessage>> responseBuilder)
+		public virtual IRequestBuilder GetRequestBuilder(HttpRequestMessage message, MediaTypeFormatterCollection formatters, Func<IRequestBuilder, Task<HttpResponseMessage>> responseBuilder)
 		{
 			return new RequestBuilder(message, formatters, responseBuilder) { Factory = this };
 		}
@@ -30,7 +30,7 @@ namespace Pathoschild.Http.Client.Default
 		/// <param name="task">The request task.</param>
 		/// <param name="formatters">The formatters used for serializing and deserializing message bodies.</param>
 		/// <param name="throwError">Whether to handle errors from the upstream server by throwing an exception.</param>
-		public IResponse GetResponse(HttpRequestMessage request, Task<HttpResponseMessage> task, MediaTypeFormatterCollection formatters, bool throwError = true)
+		public virtual IResponse GetResponse(HttpRequestMessage request, Task<HttpResponseMessage> task, MediaTypeFormatterCollection formatters, bool throwError = true)
 		{
 			return new Response(request, task, formatters, throwError);
 		}
@@ -42,7 +42,7 @@ namespace Pathoschild.Http.Client.Default
 		/// <param name="formatters">The formatters used for serializing and deserializing message bodies.</param>
 		/// <param name="contentType">The HTTP content type (or <c>null</c> to automatically select one).</param>
 		/// <exception cref="InvalidOperationException">No MediaTypeFormatters are available on the API client for this content type.</exception>
-		public MediaTypeFormatter GetFormatter(MediaTypeFormatterCollection formatters, MediaTypeHeaderValue contentType = null)
+		public virtual MediaTypeFormatter GetFormatter(MediaTypeFormatterCollection formatters, MediaTypeHeaderValue contentType = null)
 		{
 			if (!formatters.Any())
 				throw new InvalidOperationException("No MediaTypeFormatters are available on the fluent client.");
@@ -57,7 +57,7 @@ namespace Pathoschild.Http.Client.Default
 		}
 
 		/// <summary>Get the default media type formatters used by the client.</summary>
-		public MediaTypeFormatterCollection GetDefaultFormatters()
+		public virtual MediaTypeFormatterCollection GetDefaultFormatters()
 		{
 			return new MediaTypeFormatterCollection();
 		}
@@ -65,7 +65,7 @@ namespace Pathoschild.Http.Client.Default
 		/// <summary>Construct an HTTP request message.</summary>
 		/// <param name="method">The HTTP method.</param>
 		/// <param name="resource">The URI to send the request to.</param>
-		public HttpRequestMessage GetRequestMessage(HttpMethod method, Uri resource)
+		public virtual HttpRequestMessage GetRequestMessage(HttpMethod method, Uri resource)
 		{
 			return new HttpRequestMessage(method, resource);
 		}
