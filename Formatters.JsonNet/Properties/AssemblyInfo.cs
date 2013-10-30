9fb84fbccf86d582f020c7cefffca341a95b11a5
Jesse Plamondon-Willard <plamondon@brainbankinc.com>
2013-10-29 21:26:15 -0400

    Updated to latest Microsoft and JSON.NET packages; +GlobalAssembly.

--- Formatters.JsonNet/Properties/AssemblyInfo.cs
+++ Formatters.JsonNet/Properties/AssemblyInfo.cs
@@ -1,36 +1,6 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("Pathoschild.Http.Formatters.JsonNet")]
 [assembly: AssemblyDescription("Provides MediaTypeFormatters for JSON and BSON using Json.NET. These can be used by the .Net 4.5 HttpClient and the ASP.NET Web API for serializing message bodies.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Pathoschild.Http.Client")]
-[assembly: AssemblyCopyright("Copyright Jesse Plamondon-Willard 2012")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("6abdb0b9-839f-4495-95ec-f3bb0330cb3c")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ GlobalAssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Pathoschild.Http.Client")]
+[assembly: AssemblyCopyright("Copyright Jesse Plamondon-Willard 2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
