b8e3f1cd7b4e4500aafc7342f96a1eac027e2aeb
Jesse Plamondon-Willard <github@jplamondonw.com>
2016-12-09 00:30:37 -0500

    update assemblyinfo

--- Client/Properties/AssemblyInfo.cs
+++ Client/Properties/AssemblyInfo.cs
@@ -1,6 +1,6 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyProduct("Pathoschild.Http.Client")]
-[assembly: AssemblyCopyright("Copyright Jesse Plamondon-Willard 2012–2016")]
-[assembly: AssemblyVersion("2.3.0.0")]
-[assembly: AssemblyFileVersion("2.3.0.0")]
+[assembly: AssemblyCopyright("Copyright 2012–2017")]
+[assembly: AssemblyVersion("2.4.0.0")]
+[assembly: AssemblyFileVersion("2.4.0.0")]
--- Tests/Properties/AssemblyInfo.cs
+++ Tests/Properties/AssemblyInfo.cs
@@ -3,6 +3,6 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("0b18c453-907b-4e39-901e-b5c068908b7b")]
 [assembly: AssemblyProduct("Pathoschild.Http.Client")]
-[assembly: AssemblyCopyright("Copyright Jesse Plamondon-Willard 2012–2016")]
-[assembly: AssemblyVersion("2.3.0.0")]
-[assembly: AssemblyFileVersion("2.3.0.0")]
+[assembly: AssemblyCopyright("Copyright 2012–2017")]
+[assembly: AssemblyVersion("2.4.0.0")]
+[assembly: AssemblyFileVersion("2.4.0.0")]
