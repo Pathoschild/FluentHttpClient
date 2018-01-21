a79f672c02d7499b54203929b5574ae9e7382201
Jeremie Desautels <desautelsj@hotmail.com>
2018-01-21 16:19:23 -0500

    Modify UriExtensions.WithArguments to ignore null arguments if desired

--- Client/Internal/UriExtensions.cs
+++ Client/Internal/UriExtensions.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -11,15 +11,27 @@ namespace Pathoschild.Http.Client.Internal
         /// <summary>Add raw arguments to the URI's query string.</summary>
         /// <param name="uri">The URI to extend.</param>
         /// <param name="arguments">The raw arguments to add.</param>
+        /// <param name="ignoreNullArguments">Indicates whether to ignore arguments with null value.</param>
         /// <remarks>This method can't use <see cref="System.Net.Http.UriExtensions.ParseQueryString" /> because it isn't compatible with portable class libraries.</remarks>
-        public static Uri WithArguments(this Uri uri, params KeyValuePair<string, object>[] arguments)
+        public static Uri WithArguments(this Uri uri, bool ignoreNullArguments, params KeyValuePair<string, object>[] arguments)
         {
-            string queryString = string.Join("&",
+            string argumentsWithValue = string.Join("&",
                 from argument in arguments
+                where argument.Value != null
                 let key = WebUtility.UrlEncode(argument.Key)
-                let value = argument.Value != null ? WebUtility.UrlEncode(argument.Value.ToString()) : string.Empty
+                let value = WebUtility.UrlEncode(argument.Value.ToString())
                 select key + "=" + value
             );
+
+            string argumentsWithoutValue = string.Join("&",
+                from argument in arguments
+                where argument.Value == null
+                let key = WebUtility.UrlEncode(argument.Key)
+                select key + "="
+            );
+
+            string queryString = argumentsWithValue + (!ignoreNullArguments && !string.IsNullOrEmpty(argumentsWithoutValue) ? "&" + argumentsWithoutValue : string.Empty);
+
             return new Uri(
                 uri
                 + (string.IsNullOrWhiteSpace(uri.Query) ? "?" : "&")
